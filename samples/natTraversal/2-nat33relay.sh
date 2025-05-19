7b234e6cff024d60cdb56beac45098f355c9e37a
Yang Chen <yang.chen@linuxe.org>
2025-05-19 11:28:19 +0800

    Support 2-nat33relay.sh as entry.sh for oneclick deploy.
    Please set envirounment for container running.
    Checkout script for more.

--- samples/natTraversal/2-nat33relay.sh
+++ samples/natTraversal/2-nat33relay.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 PATH=/usr/sbin:/usr/bin:/sbin:/bin
+#Envirounment
+[ "v$N4HOST" = "v" ] && N4HOST=11.22.33.44
+[ "v$N4PORT" = "v" ] && N4PORT=12710
+[ "v$DESTIP" = "v" ] && DESTIP=8.8.8.8
+[ "v$DESTPORT" = "v" ] && DESTPORT=9999
+VERSION=v1.3.0
+
+#Download binary
+pkill udpdeminer
+if [ ! -x ./udpdeminer ]; then
+	curl "https://github.com/basncy/udpdeminer-binary/releases/download/${VERSION}/udpdeminer-x86_64-unknown-linux-musl" --location --retry-connrefused --retry 10 --fail -s -o udpdeminer
+	#curl "https://github.com/basncy/udpdeminer-binary/releases/download/${VERSION}/udpdeminer-aarch64-unknown-linux-musl" --location --retry-connrefused --retry 10 --fail -s -o udpdeminer
+        chmod +x udpdeminer
+fi
+if [ ! -x ./n4.py ]; then
+	curl "https://raw.githubusercontent.com/basncy/udpdeminer-binary/refs/heads/main/samples/natTraversal/n4.py" --location --retry-connrefused --retry 10 --fail -s -o n4.py
+	chmod +x n4.py
+fi
 
 while true; do
         KILLPID=$FWDPID
-        MYSTR=$(python3 n4.py -c -x -h 11.22.33.44 -p 12710 -b $((RANDOM + RANDOM + 1025)))
+        MYSTR=$(python3 n4.py -c -x -h $N4HOST -p $N4PORT -b $((RANDOM + RANDOM + 1025)))
         (sleep 10 && kill -9 $KILLPID) &
         LPORT=$(echo $MYSTR|cut -d'-' -f2)
-        ./udpdeminer-x86_64-unknown-linux-musl -b 0.0.0.0 -l $LPORT --server 8.8.8.8 --port 9999 --outbound 0.0.0.0 & export FWDPID=$!
-        #./udpdeminer-aarch64-unknown-linux-musl -b 0.0.0.0 -l $LPORT --server 8.8.8.8 --port 9999 --outbound 0.0.0.0 & FWDPID=$!
-        #socat "udp-listen:$LPORT" 'udp6:[2001::8888]:9999' & FWDPID=$!
+        ./udpdeminer -b 0.0.0.0 -l $LPORT --server $DESTIP --port $DESTPORT --outbound 0.0.0.0 & export FWDPID=$!
+	#socat "udp-listen:$LPORT" 'udp6:[2603:c020:4:1200::a00:232]:10000' & FWDPID=$!
 done
